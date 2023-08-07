74fc0da5de22e05c954d1ece50ad6a06479838b3
Jameson <james.bradlee@telenor.no>
2023-08-07 04:00:51 +0200

    remove version notice

--- src/action.ts
+++ src/action.ts
@@ -1,6 +1,5 @@
-import { getInput, string, boolean, notice } from "jamesons-actions-toolkit";
+import { getInput, string, boolean } from "jamesons-actions-toolkit";
 import incrementVersionForRepository from "./lib/incrementVersionForRepository";
-import version from "./version";
 
 export default async function action() {
 	const token = getInput("token", { optional: false, type: string });
@@ -8,8 +7,6 @@ export default async function action() {
 	const type = getInput("type", { optional: false, type: string });
 	const vprefix =
 		getInput("vprefix", { optional: true, type: boolean }) ?? true;
-
-	notice("Using auto-semver@%s", version);
 
 	await incrementVersionForRepository(
 		`https://github-actions:${token}@github.com/${repository}.git`,
